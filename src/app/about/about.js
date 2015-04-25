7ffc053a726bae59da661cf4628a292fea1cd7ea
Alex Samuel <alex@alexsamuel.net>
2015-04-25 15:56:06 -0400

    Rename the package.

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -1,4 +1,4 @@
-angular.module( 'ngBoilerplate.about', [
+angular.module( 'supdoc.about', [
   'ui.router',
   'placeholders',
   'ui.bootstrap'
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,8 @@
-angular.module('ngBoilerplate', [
+angular.module('supdoc', [
   'templates-app',
   'templates-common',
-  'ngBoilerplate.home',
-  'ngBoilerplate.about',
+  'supdoc.home',
+  'supdoc.about',
   'ui.router'
 ])
 
